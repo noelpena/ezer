22427aff84e42f9bb9e9cba4c72adbf2a08a2556
penan-pbsc <penan@PF3DY7LW>
2022-09-26 18:31:21 -0400

    added view pages and dept endpoints

---
+++ pages/api/departments/create.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+	if(req.method !== 'POST'){
+		return res.status(405).json({message:'Method not allowed'})
+	} 
+
+	const body = JSON.parse(req.body);
+	const {name} = body
+	const newDept = await prisma.department.create({
+		data: {
+			name: name,
+			is_active: true,
+		},
+		select: {
+			id: false,
+			name: true,
+			is_active: true,
+			created_at:false,
+			last_modified: false,        
+			account_type: false,
+			balance:false,
+			records:false
+		}
+	});
+
+	res.status(200).json({
+		message: "Record added",
+		data: newDept
+	})
+
+}
