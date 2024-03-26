a397be61e7655313195c6e0b75a8c7cd60adc21c
Noel Pena <>
2024-03-25 20:44:13 -0400

    Added logic for otp code in login

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Auth } from "@supabase/auth-ui-react";
+import { Auth, MagicLink } from "@supabase/auth-ui-react";
 import { ThemeSupa, ViewType } from "@supabase/auth-ui-shared";
 
 import {
@@ -19,13 +19,22 @@ const inter = Inter({ subsets: ["latin"] });
 type AppProps = {
 	session: Session | null;
 	auth_view: ViewType;
+	_email: string;
 };
 
-const Home = ({ session, auth_view }: AppProps) => {
+const Home = ({ session, auth_view, _email }: AppProps) => {
 	const router = useRouter();
 	const [sesh, setSesh] = useState(session);
 	const [authView, setAuthView] = useState<ViewType>(auth_view);
+	const [email, setEmail] = useState<string>(_email);
 	const supabase = createSupabaseFrontendClient();
+
+	useEffect(() => {
+		const emailField = document.getElementById("email") as HTMLInputElement;
+		if (email !== "") {
+			emailField.value = email;
+		}
+	}, []);
 
 	useEffect(() => {
 		const { data } = supabase.auth.onAuthStateChange(
@@ -74,8 +83,26 @@ const Home = ({ session, auth_view }: AppProps) => {
 								providers={[]}
 								showLinks={false}
 								view={authView}
+								// view={"verify_otp"}
+								localization={{
+									variables: {
+										magic_link: {
+											button_label:
+												"Send login link to your email address",
+											loading_button_label:
+												"Sending login link...",
+										},
+										verify_otp: {
+											email_input_placeholder:
+												email || "Enter your email",
+											token_input_label: "Login code",
+											token_input_placeholder:
+												"Your login code",
+										},
+									},
+								}}
 							/>
-							{authView == "sign_in" ? (
+							{authView == "sign_in" && (
 								<Text
 									className="text-center text-stone-500"
 									size="sm"
@@ -88,13 +115,32 @@ const Home = ({ session, auth_view }: AppProps) => {
 										Send login link to your email address
 									</a>
 								</Text>
-							) : (
+							)}
+
+							{authView == "magic_link" && (
 								<Text
 									className="text-center text-stone-500"
 									size="sm"
 								>
 									<a
 										onClick={() => changeAuth("sign_in")}
+										className="cursor-pointer text-blue-500"
+									>
+										Login using email and password
+									</a>
+								</Text>
+							)}
+
+							{authView == "verify_otp" && (
+								<Text
+									className="text-center text-stone-500"
+									size="sm"
+								>
+									<a
+										onClick={() => {
+											changeAuth("sign_in");
+											router.push("/");
+										}}
 										className="cursor-pointer text-blue-500"
 									>
 										Login using email and password
@@ -118,12 +164,16 @@ export default Home;
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 	const supabase = createSupabaseReqResClient(ctx);
 	const auth_view = ctx.query.view || "sign_in";
+	let _email = ctx.query.email || "";
+	if (_email) {
+		_email = decodeURIComponent(_email as string);
+	}
 
 	const {
 		data: { session },
 	} = await supabase.auth.getSession();
 
 	return {
-		props: { session, auth_view },
+		props: { session, auth_view, _email },
 	};
 };
